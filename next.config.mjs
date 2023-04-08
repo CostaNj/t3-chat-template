c6f74ab6ab9d4736aa3e3972ddee70285ab8d0ca
CostaNj <costa.n.work@yandex.ru>
2023-04-08 04:34:34 +0300

    add linaria config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
+// @ts-ignore
+import withLinaria from 'next-linaria';
+
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
 /** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-};
+const config = withLinaria({
+	reactStrictMode: true,
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
+	},
+});
 export default config;
