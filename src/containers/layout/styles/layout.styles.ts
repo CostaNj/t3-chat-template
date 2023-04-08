795c8cb602c2c6b09f97d049a0bc297e569986ce
CostaNj <costa.n.work@yandex.ru>
2023-04-08 05:04:27 +0300

    remove media

--- src/containers/layout/styles/layout.styles.ts
+++ src/containers/layout/styles/layout.styles.ts
@@ -9,25 +9,4 @@ export const Container = styled.main`
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-
-	@media (min-width: 640px) {
-		max-width: 640px;
-	}
-
-	@media (min-width: 768px) {
-		max-width: 768px;
-	}
-
-	@media (min-width: 1024px) {
-		max-width: 1024px;
-	}
-
-
-	@media (min-width: 1280px) {
-		max-width: 1280px;
-	}
-
-	@media (min-width: 1536px) {
-		max-width: 1536px;
-	}
 `
